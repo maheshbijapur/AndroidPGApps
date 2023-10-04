17041857b8806d4f38f7e8e150bc3a2b72a393dc
Mahesh Bijapur <glo88070@adobe.com>
2023-10-04 18:30:46 +0530

    added more sample apps

---
+++ Archive/ChinaMotors/chinamotors/www/js/index.js
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+    marketo.initialize(
+  function() { console.log("MarketoSDK Init done."); },
+  function(error) { console.log("an error occurred:" + error); },
+  '578-BOD-529',
+  'ZmNxQnNtNUdKaFhaMVE2dXdXb3BVazRh'
+);
+ 
+
+marketo.initializeMarketoPush(
+    function() { console.log("Marketo push successfully initialized."); },
+    function(error) { console.log("an error occurred:" + error); },
+    '350312872033' // This is required for Android and will be ignored in iOS
+);
+
+var lead = {};
+lead[marketo.KEY_FIRST_NAME] = "Phone";
+lead[marketo.KEY_LAST_NAME] = "Gap";
+lead[marketo.KEY_EMAIL] = email;
+lead[marketo.KEY_ADDRESS] = "demo address";
+lead[marketo.KEY_CITY] = "city";
+lead[marketo.KEY_STATE] = "state";
+lead[marketo.KEY_COUNTRY] = "country";
+lead[marketo.KEY_POSTAL_CODE] = "postalCode";
+lead[marketo.KEY_GENDER] = "gender";
+// To use lead custom field, use the REST API NAME as key
+lead["REST API NAME"] = "value";
+ 
+// Use associateLead function to associate it.
+marketo.associateLead(
+  function() { console.log("MarketoSDK : Lead Associated"); },
+  function(error) { console.log("an error occurred:" + error); },
+  JSON.stringify(lead)
+);
+
+}
